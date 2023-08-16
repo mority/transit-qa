eb42250c07a7b5fa312c6d2c9f2f0605b837afd8
Felix Gündling <felix.guendling@gmail.com>
2023-08-16 11:32:09 +0200

    change rating logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,11 @@
 
     $: conns1 = [
         new Connection(
+            '09:00',
+            '11:00',
+            3
+        ),
+        new Connection(
             '10:00',
             '12:00',
             3
@@ -21,6 +26,31 @@
     ];
 
     $: conns2 = [
+        // new Connection(
+        //     '10:00',
+        //     '12:00',
+        //     3
+        // ),
+        // new Connection(
+        //     '11:00',
+        //     '13:00',
+        //     3
+        // ),
+        // new Connection(
+        //     '12:00',
+        //     '14:00',
+        //     3
+        // ),
+        // new Connection(
+        //     '12:00',
+        //     '14:00',
+        //     3
+        // ),
+        new Connection(
+            '09:00',
+            '11:00',
+            3
+        ),
         new Connection(
             '10:00',
             '12:00',
@@ -29,8 +59,13 @@
         new Connection(
             '11:00',
             '13:00',
-            3
+            6
         ),
+        // new Connection(
+        //     '13:00',
+        //     '15:00',
+        //     3
+        // ),
         new Connection(
             '12:00',
             '14:00',
@@ -38,99 +73,43 @@
         )
     ];
 
-    // Preference vectors:
-    // [travel time weight, transfer weight]
-    const C = [
-        [1.0, 30.0],
-        [1.0, 1.0],
-        [0.0, 1.0]
-    ];
+    function getImprovement(a: Connection, b: Connection, weights: number[]) {
+        const criteriaA = a.getCriteria3();
+        const criteriaB = b.getCriteria3();
 
-    function f2(c: number[], x: number[]) {
-        return x[0] * c[0] + x[1] * c[1];
+        let dist = 0.0;
+        let improvement = 0.0;
+
+        for (let i = 0; i != weights.length; ++i) {
+            const weightedA = criteriaA[i] * weights[i];
+            const weightedB = criteriaB[i] * weights[i];
+            const criterionDist = weightedA - weightedB;
+
+            dist += Math.pow(criterionDist, 2);
+            if (criterionDist < 0) {
+                improvement += Math.pow(criterionDist, 2);
+            }
+        }
+
+        dist = Math.sqrt(dist);
+        improvement = Math.sqrt(improvement);
+
+        return {dist, improvement};
     }
 
-    function best(c: number[], conns: Connection[]): Connection {
-        let best = null;
-        let bestVal = Number.MAX_VALUE;
-        conns.forEach(conn => {
-            const val = f2(c, conn.getCriteria2());
-            if (val < bestVal) {
-                bestVal = val;
-                best = conn;
+    function getMinImprovement(conn: Connection, X: Connection[], weights: number[]) {
+        let minImprovement = Number.MAX_VALUE;
+        let min = null;
+
+        X.forEach(x => {
+            let {dist, improvement} = getImprovement(conn, x, weights);
+            if (improvement < minImprovement) {
+                minImprovement = improvement;
+                min = x;
             }
         });
-        return best;
-    }
 
-    function rate(conns: Connection[]) {
-        let sum = 0.0;
-        for (let c in C) {
-            const b = best(C[c], conns);
-            sum += f2(C[c], b.getCriteria2());
-        }
-        return sum;
-    }
-
-    function getRelTimeCorrection(a: Connection, b: Connection, ca: number, cb: number) {
-        const alpha = 0.5;
-
-        const depA = a.getDeparture();
-        const depB = b.getDeparture();
-        const arrA = a.getArrival();
-        const arrB = b.getArrival();
-        console.log("param: ", {ca}, {cb}, {depA}, {depB}, {arrA}, {arrB});
-
-        const omega = a.overtakes(b) ? 0 : Number.MAX_VALUE;
-        const distance = Math.min(Math.abs(depA - depB), Math.abs(arrA - arrB), omega);
-        console.log({omega}, {distance});
-
-        return alpha * (ca / cb) * distance;
-    }
-
-    function getMaxImprovment(A: Connection[], B: Connection[], weights: number[]) {
-        let maxImprovement = -1;
-        let maxA = null, maxB = null;
-
-        A.forEach(cA => {
-            B.forEach(cB => {
-                const criteriaA = cA.getCriteria2();
-                const criteriaB = cB.getCriteria2();
-
-                let dist = 0.0;
-                let improvement = 0.0;
-
-                for (let i = 0; i != weights.length; ++i) {
-                    const weightedA = criteriaA[i] * weights[i];
-                    const weightedB = criteriaB[i] * weights[i];
-                    const correction = getRelTimeCorrection(cA, cB, weightedA, weightedB);
-                    const criterionDist = weightedA + correction - weightedB;
-
-                    dist += Math.pow(criterionDist, 2);
-                    if (criterionDist < 0) {
-                        improvement += Math.pow(criterionDist, 2);
-                    }
-
-                    console.log({i}, {weightedA}, {weightedB}, {correction}, {criterionDist});
-                }
-
-                dist = Math.sqrt(dist);
-                improvement = Math.sqrt(improvement);
-
-                if (improvement >= maxImprovement) {
-                    maxImprovement = improvement;
-                    maxA = cA;
-                    maxB = cB;
-                }
-
-                // console.log({cA}, '\n', {cB}, '\n\t', {dist}, {improvement}, '\n\n\n');
-            });
-        });
-
-        console.assert(maxA != null, "maxA == null");
-        console.assert(maxB != null, "maxA == null");
-
-        return {maxImprovement, maxA, maxB};
+        return {minImprovement, min};
     }
 
     function getSetImprovement(A: Connection[], B: Connection[], weights: number[]) {
@@ -142,24 +121,26 @@
             return Number.MAX_VALUE;
         }
 
-        let improvement = 0;
+        let aCopy = [...A];
+        let bCopy = [...B];
+        let improvement = 0.0;
 
-        let aCopy = [... A];
-        let bCopy = [... B];
-
-        console.log({aCopy, bCopy});
-
-        let i = 0;
         while (aCopy.length !== 0) {
-            let {maxImprovement, maxA, maxB} = getMaxImprovment(aCopy, bCopy, weights);
-            console.log({maxImprovement, maxA, maxB})
-            bCopy.push(maxA);
-            aCopy.splice(aCopy.indexOf(maxA), 1);
-            console.log({aCopy, bCopy});
-            improvement += maxImprovement;
-            if (maxImprovement <= 0.1 || ++i > 10) {
-                break;
-            }
+            let minImprovementA = Number.MAX_VALUE;
+            let minA = null, minB = null;
+
+            aCopy.forEach(a => {
+                let {minImprovement, min} = getMinImprovement(a, bCopy, weights);
+                if (minImprovement < minImprovementA) {
+                    minImprovementA = minImprovement;
+                    minA = a;
+                    minB = min;
+                }
+            });
+
+            improvement += minImprovementA;
+            aCopy.splice(aCopy.indexOf(minA), 1);
+            bCopy.push(minA);
         }
 
         return improvement;
@@ -177,9 +158,11 @@
 </div>
 
 <div class="text-center" style="font-family: monospace;">
-    sum1: {rate(conns1)}<br>
-    sum2: {rate(conns2)}<br>
-    max improvement: {getMaxImprovment(conns1, conns2, [1, 30]).maxImprovement}<br>
-    set improvement LR: {getSetImprovement(conns1, conns2, [1, 30])}<br>
-    set improvement RL: {getSetImprovement(conns2, conns1, [1, 30])}
+    set improvement LR: {getSetImprovement(conns1, conns2, [1, 1, 30]).toFixed(1)}<br>
+    set improvement RL: {getSetImprovement(conns2, conns1, [1, 1, 30]).toFixed(1)}<br>
+    set improvement diff
+    LR: {(getSetImprovement(conns1, conns2, [1, 1, 30]) - getSetImprovement(conns2, conns1, [1, 1, 30])).toFixed(1)}<br>
+    set improvement diff
+    RL: {(getSetImprovement(conns2, conns1, [1, 1, 30]) - getSetImprovement(conns1, conns2, [1, 1, 30])).toFixed(1)}
+    <br>
 </div>
