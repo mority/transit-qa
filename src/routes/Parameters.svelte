0470d7b3a79424a07ebc40ec1074465be05e68f5
mority <mority@posteo.net>
2024-10-01 11:15:30 +0200

    formatting

--- src/routes/Parameters.svelte
+++ src/routes/Parameters.svelte
@@ -27,45 +27,46 @@
 	<table class="table-auto w-full">
 		<tbody class="text-sm divide-y divide-slate-200">
 			<tr>
-				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500"
-					>Schutzfaktor</td
-				>
+				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500">
+					Schutzfaktor
+				</td>
 				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
 					<input type="number" bind:value={params.alpha} class="border-0 w-16" />
 				</td>
 			</tr>
 			<tr>
-				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500"
-					>Basis Taxi</td
-				>
+				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500">
+					Basis Taxi
+				</td>
 				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
 					<input type="number" bind:value={params.baseTaxi} class="border-0 w-16" />
 				</td>
 			</tr>
 			<tr>
-				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500"
-					>Faktor Taxi</td
-				>
+				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500">
+					Faktor Taxi
+				</td>
 				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
 					<input type="number" bind:value={params.factorTaxi} class="border-0 w-16" />
 				</td>
 			</tr>
 			<tr>
-				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500"
-					>Faktor Umstieg</td
-				>
+				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500">
+					Faktor Umstieg
+				</td>
 				<td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
 					<input type="number" bind:value={params.factorTransfer} class="border-0 w-16" />
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2" class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500"
-					><label for="json-io">JSON Import/Export</label><br /><textarea
-						id="json-io"
-						style="width: 100%; height: 200px;"
-						bind:value={jsonIo}
-					></textarea></td
+				<td
+					colspan="2"
+					class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap decoration-pink-500"
 				>
+					<label for="json-io">JSON Import/Export</label>
+					<br />
+					<textarea id="json-io" style="width: 100%; height: 200px;" bind:value={jsonIo}></textarea>
+				</td>
 			</tr>
 		</tbody>
 	</table>
